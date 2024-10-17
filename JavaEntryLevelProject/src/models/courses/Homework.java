a47cd1bf69148fdacfd6a155cc5bd3fc408d518d
Konstantin Nikolov <knikolov@astea.net>
2024-10-18 01:25:52 +0300

    Refactor Homework model

---
+++ JavaEntryLevelProject/src/models/courses/Homework.java
@@ -0,0 +1,79 @@
+package models.courses;
+
+import java.time.LocalDate;
+
+public class Homework {
+    private int id;
+    private String title;
+    private String description;
+    private int courseId;
+    private int studentId;
+    private LocalDate dueDate;
+    private double grade;
+
+    public Homework(int id, String title, String description, int courseId, int studentId, LocalDate dueDate, double grade) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.courseId = courseId;
+        this.studentId = studentId;
+        this.dueDate = dueDate;
+        this.grade = grade;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getCourseId() {
+        return courseId;
+    }
+
+    public void setCourseId(int courseId) {
+        this.courseId = courseId;
+    }
+
+    public int getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(int studentId) {
+        this.studentId = studentId;
+    }
+
+    public LocalDate getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(LocalDate dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public double getGrade() {
+        return grade;
+    }
+
+    public void setGrade(double grade) {
+        this.grade = grade;
+    }
+}
